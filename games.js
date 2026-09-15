8fee1c155130f5bef05ba636a1e02d23601798cb
Bmowarmachine <brandonbbenavides@gmail.com>
2026-09-15 14:34:38 -0600

    Update games.js

--- games.js
+++ games.js
@@ -20,7 +20,7 @@ window.NFC_GAMES = {
     core: "gb",
     icon: "🔵",
     description: "Llavero NFC de Pokémon Blue.",
-    romUrl: ""
+    romUrl: "./roms/Pokemon - Blue Version(USA,Europe)(SGB Enhanced).gb"
   },
 
   "zelda": {
